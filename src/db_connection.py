75872ce9490f0a5854e58a408f82a7845c87025e
YeabsiraNigusse <nigusseyeabsira@gmail.com>
2025-06-14 21:19:10 +0300

    added .env

--- src/db_connection.py
+++ src/db_connection.py
@@ -1,6 +1,22 @@
 import cx_Oracle
+import os
+from dotenv import load_dotenv
+
+# Load .env variables
+load_dotenv()
 
 def get_connection():
-    dsn = cx_Oracle.makedsn("localhost", 1521, service_name="XE")  # Replace XE if yours is different
-    conn = cx_Oracle.connect(user="system", password="admin", dsn=dsn)
+    dsn = cx_Oracle.makedsn(
+        os.getenv("DB_HOST"),
+        os.getenv("DB_PORT"),
+        service_name=os.getenv("DB_SERVICE_NAME")
+    )
+    conn = cx_Oracle.connect(
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        dsn=dsn
+    )
+    print("✅ Connection successful!")
     return conn
+
+get_connection()
